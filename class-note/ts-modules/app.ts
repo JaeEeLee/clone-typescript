779e76aeed3ed7d49d4d3c2e03ce503b65b1acc5
LeeJaeEe <wodl0504@naver.com>
2021-03-28 20:50:03 +0900

    시스템 모듈화 적용 실습

---
+++ class-note/ts-modules/app.ts
@@ -0,0 +1,11 @@
+// interface Todo {
+// 	title: string
+// 	checked: boolean
+// }
+
+import { Todo } from './types'
+
+let item: Todo = {
+	title: 'todo 1',
+	checked: false,
+}
---
+++ quiz/2_address-book/src/types.ts
@@ -0,0 +1,19 @@
+export { Contact, PhoneType }
+
+interface PhoneNumberDictionary {
+	[phone: string]: {
+		num: number;
+	};
+}
+
+interface Contact {
+	name: string;
+	address: string;
+	phones: PhoneNumberDictionary;
+}
+
+enum PhoneType {
+	Home = 'home',
+	Office = 'office',
+	Studio = 'studio'
+}
